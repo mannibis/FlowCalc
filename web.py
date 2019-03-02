e6f80c1a9c2563c1ba1fdd01a15509ed32051767
mannibis <esm2107@gmail.com>
2019-03-01 20:59:33 -0500

    updated html formatting and styles

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/calc', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def calc():
     form = CalcForm()
 
@@ -22,14 +22,13 @@ def calc():
 
         flow_rate_lam_imp = None
         flow_rate_turb_imp = None
-        velocity_imp = None
+        # velocity_imp = None
         reynolds_number = None
 
         try:
             flow_rate_lam_metric = pois_metric(pipe_diameter_metric, delta_p_metric, pipe_length_metric)
             flow_rate_lam_imp = "%.2f" % Decimal(flow_rate_lam_metric * 15850.3)
 
-            # flash("Q (laminar) = %.2f gpm" % Decimal(flow_rate_lam_imp))
         except ValueError:
             flash("There was a math value error. Check your numbers and try again.")
         except TypeError:
@@ -37,12 +36,10 @@ def calc():
 
         try:
             flow_rate_turb_metric = bern_metric(pipe_diameter_metric, delta_p_metric, pipe_length_metric)[0]
-            velocity_metric = bern_metric(pipe_diameter_metric, delta_p_metric, pipe_length_metric)[1]
             flow_rate_turb_imp = "%.2f" % Decimal(flow_rate_turb_metric * 15850.3)
-            velocity_imp = "%.2f" % Decimal(velocity_metric * 3.28084)
+            # velocity_metric = bern_metric(pipe_diameter_metric, delta_p_metric, pipe_length_metric)[1]
+            # velocity_imp = "%.2f" % Decimal(velocity_metric * 3.28084)
 
-            # flash("Q (turbulent) = %.2f gpm" % Decimal(flow_rate_turb_imp))
-            # flash("V (turbulent) = %.2f ft/s" % Decimal(velocity_imp))
         except ValueError:
             flash("There was a math value error. Check your numbers and try again.")
         except TypeError:
@@ -55,10 +52,10 @@ def calc():
         except TypeError:
             flash("There was a type error. Make sure you are entering numbers without units and try again.")
 
-        return render_template('index.html', title='Calculate Flowrate', form=form, execute=True,
-                               q_lam_imp=flow_rate_lam_imp, q_turb_imp=flow_rate_turb_imp, v_turb_imp=velocity_imp,
+        return render_template('index.html', title='Flowrate Calculator', form=form, execute=True,
+                               q_lam_imp=flow_rate_lam_imp, q_turb_imp=flow_rate_turb_imp,
                                r_num=reynolds_number)
-    return render_template('index.html', title='Calculate Flowrate', form=form, execute=False)
+    return render_template('index.html', title='Flowrate Calculator', form=form, execute=False)
 
 
 if __name__ == '__main__':
